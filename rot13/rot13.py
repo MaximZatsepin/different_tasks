1a714db92fdd786bd466e4eb614436bd9490de3d
Maxim <130181963+MaximZatsepin@users.noreply.github.com>
2023-09-09 15:08:33 +0300

    rot13

---
+++ rot13/rot13.py
@@ -0,0 +1,16 @@
+def rot13(message):
+    newMessage = ''
+    for i in message:
+        if i.isalpha():
+            if i.islower():
+                char = (ord(i)+13) - 123 + 97 if (ord(i)+13) > 122 else (ord(i)+13)
+                newMessage += chr(char)
+            else: # if i upper
+                char = (ord(i)+13) - 123 + 97 if (ord(i)+13) > 90 else (ord(i)+13)
+                newMessage += chr(char)
+        else:
+            newMessage += i
+    return(newMessage)
+
+print(rot13("EBG13 rknzcyr.")) # -> "ROT13 example."
+print(rot13("@[`{")) # -> "@[`{"
