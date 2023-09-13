657d8139835d128808e62a7b45b83ac998400dd6
Maxim <130181963+MaximZatsepin@users.noreply.github.com>
2023-09-13 21:42:11 +0300

    Update LinkedList.py

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -1,31 +1,70 @@
-class Node:
-    def __init__(self,data) -> None:
-        self.data = data
-        self.next = None
+class Node: # Класс-Узел. Внутри лежит дата и переменная-адрес следующего узла
+    def __init__(self,data,next = None) -> None: 
+        self.data = data # Дата - число, строка, тдтп
+        self.next = next # Next - "ссылка" на следующий
 
 class LinkedList:
     def __init__(self) -> None:
-        self.head = None
-    
-    def placeNext(self,data) -> None:
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-            return
-        
+        self.head = None # Голова листа (смотри ниже, как работает)
+        #self.size = 0
+
+    def appendToStart(self,data) -> None:
+        newNode = Node(data,self.head) # Создаем новую ноду с переданной data
+        self.head = newNode # Голова двигается вправо
+
+    def appendToEnd(self,data) -> None:
         currNode = self.head
-        while(currNode.next != None):
+        while(currNode.next):
             currNode = currNode.next
-        currNode.next = new_node
-    
+        newNode = Node(data,None)
+        currNode.next = newNode
+
+    def removeFromEnd(self) -> None:
+        currNode = self.head
+        while(currNode.next.next):
+            currNode = currNode.next
+        currNode.next = None
+
+    def removeFromValue(self,data) -> None:
+        currNode = self.head
+        if currNode.data == data:
+            currNode.next = currNode.next.next
+            self.head = currNode.next
+            return
+        while(currNode.next.data != data):
+            currNode = currNode.next
+        print(f'currNode: {currNode.data}')
+        currNode.next = currNode.next.next
+
     def printLList(self) -> None:
         currNode = self.head
-        while currNode:
-            print(currNode.data)
+        outputArr = []
+        while(currNode):
+            outputArr.append(currNode.data)
             currNode = currNode.next
+        print(outputArr)
+
+newLList = LinkedList()
+newLList.appendToStart(2)
+newLList.appendToEnd(1)
+newLList.appendToStart(5)
+newLList.appendToEnd(3)
+newLList.appendToStart(7)
+# newLList.removeFromEnd()
+newLList.removeFromValue(7)
 
 
-myLlist = LinkedList()
-myLlist.placeNext(2)
-myLlist.placeNext(3)
-myLlist.printLList()
+newLList.printLList()
+
+'''
+Данный лист работает по такой схеме:
+-
+0
+0-
+-0
+-0-
+--0
+--0-
+---0
+Где 0: self.head, -:Node
+'''
